45bb41c48b282410bbabb2d089a8fea49227a7b8
davestroud <david@davidstroud.me>
2020-03-11 08:54:01 -0500

    reword comments

--- NumPy matrix math 2.py
+++ NumPy matrix math 2.py
@@ -16,17 +16,23 @@ import matplotlib.cm as cm  #https://matplotlib.org/api/cm_api.html
 
 # Decision making with Matrices
 
-# This is a pretty simple assingment.  You will do something you do everyday, but today it will be with matrix manipulations.
+# This is a pretty simple assignment. You will do something you do everyday, but today 
+# it will be with matrix manipulations.
 
-# The problem is: you and your work firends are trying to decide where to go for lunch. You have to pick a resturant thats best for everyone.  
+# The problem is: you and your work friends are trying to decide where to go for lunch. 
+# You have to pick a resturant thats best for everyone.  
+
 # Then you should decided if you should split into two groups so eveyone is happier.
 
-# Displicte the simplictiy of the process you will need to make decisions regarding how to process the data.
+# Despite the simplictiy of the process you will need to make decisions regarding 
+# how to process the data.
 
-# This process was thoughly investigated in the operation research community.  This approah can prove helpful on any number of 
-# decsion making problems that are currently not leveraging machine learning.
+# This process was thoughly investigated in the operation research community.  
+# This approach can prove helpful on any number of decicison making problems 
+# that are currently not leveraging machine learning.
 
-# You asked your 10 work friends to answer a survey. They gave you back the following dictionary object.
+# You asked your 10 work friends to answer a survey. 
+# They gave you back the following dictionary object.
 
 
 #to determine random values for weights
